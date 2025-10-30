8a75895152539d049947c6af058139c84647b1ad
kento9941 <kento9941@gmail.com>
2025-10-30 14:11:30 +1030

    feat: added track settings for hihat

--- src/components/drums/drum-sequencer.js
+++ src/components/drums/drum-sequencer.js
@@ -13,50 +13,36 @@ import CrashCymbal from "./crash-cymbal";
 
 export default function DrumSequencer() {
     return (
-        <div className="row justify-content-start align-content-start">
-            <div className="col-4">
-                <div>Hihat</div>
-                <div>Open Hihat</div>
-                <div>Snare Drum</div>
-                <div>Rim Shot</div>
-                <div>Low Tom</div>
-                <div>Middle Tom</div>
-                <div>High Tom</div>
-                <div>Ride Cymbal</div>
-                <div>Crash Cymbal</div>
-                <div>Bass Drum</div>
+        <div className="">
+            <div className="mt-3">
+                <Hihat />
             </div>
-            <div className="col-8">
-                <div className="mt-3">
-                    <Hihat />
-                </div>
-                <div className="mt-3">
-                    <OpenHihat />
-                </div>
-                <div className="mt-3">
-                    <SnareDrum />
-                </div>
-                <div className="mt-3">
-                    <RimShot />
-                </div>
-                <div className="mt-3">
-                    <LowTom />
-                </div>
-                <div className="mt-3">
-                    <MiddleTom />
-                </div>
-                <div className="mt-3">
-                    <HighTom />
-                </div>
-                <div className="mt-3">
-                    <RideCymbal />
-                </div>
-                <div className="mt-3">
-                    <CrashCymbal />
-                </div>
-                <div className="mt-3">
-                    <BassDrum />
-                </div>
+            <div className="mt-3">
+                <OpenHihat />
+            </div>
+            <div className="mt-3">
+                <SnareDrum />
+            </div>
+            <div className="mt-3">
+                <RimShot />
+            </div>
+            <div className="mt-3">
+                <LowTom />
+            </div>
+            <div className="mt-3">
+                <MiddleTom />
+            </div>
+            <div className="mt-3">
+                <HighTom />
+            </div>
+            <div className="mt-3">
+                <RideCymbal />
+            </div>
+            <div className="mt-3">
+                <CrashCymbal />
+            </div>
+            <div className="mt-3">
+                <BassDrum />
             </div>
         </div>
     )
--- src/components/drums/hihat.js
+++ src/components/drums/hihat.js
@@ -12,18 +12,43 @@ export default function Hihat() {
     };
 
     return (
-        <div style={{ display: "flex", gap: "0.1rem" }}>
-            {struct.map((note, i) => (
-            <div
-                key={i}
-                onClick={() => toggleNote(i)}
-                className="drum-bar"
-                style={{
-                backgroundColor: note === "hh" ? "white" : "#171717",
-                transition: "background-color 0.10s",
-                }}
-            />
-            ))}
+        <div style={{ display: "flex" }}>
+            <div className="drum-settings">
+                <div className="name">Hi-Hat</div>
+
+                {/* mute button */}
+                <div
+                    className="mute-button"
+                    onClick={() => updateDrum("hihat", { play: !play })}
+                >
+                    {play ? <i className="fa-solid fa-volume-high" /> : <i className="fa-solid fa-volume-xmark" /> }
+                </div>
+
+                {/* volume slider */}
+                <input
+                    className="volume-slider"
+                    type="range"
+                    min="0"
+                    max="1"
+                    step="0.05"
+                    value={gain}
+                    onChange={(e) => updateDrum("hihat", { gain: parseFloat(e.target.value) })}
+                />
+                
+            </div>
+            <div style={{ display: "flex", gap: "0.1rem" }}>
+                {struct.map((note, i) => (
+                <div
+                    key={i}
+                    onClick={() => toggleNote(i)}
+                    className="drum-bar"
+                    style={{
+                    backgroundColor: note === "hh" ? "white" : "#171717",
+                    transition: "background-color 0.10s",
+                    }}
+                />
+                ))}
+            </div>
         </div>
     );
     }
